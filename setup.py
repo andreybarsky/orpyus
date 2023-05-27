4983e8e4ac3489244bf086d409bd1a88e1abc8e5
Dima Barsky <ghub@kappa.ac93.org>
2023-05-27 19:28:00 +0100

    Added setup.py, moved all python files to src/

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='orpyus',
+      version='1.0',
+      description='A python library for handling music theory concepts and assisting music analysis',
+      author='Andrey Barsky',
+      author_email='andrey.barsky@gmail.com',
+      install_requires=['numpy', 'scipy', 'matplotlib', 'sounddevice'],
+      extras_require={
+        'dev': [ 'pytest', 'ipdb' ]
+      },
+      package_dir = {'orpyus': 'src'}
+     )
