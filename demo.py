59b4575701d2546c384d59679f499e096df8d285
andrey <andreybarsky@gmail.com>
2023-05-30 23:40:10 +0100

    demo script that just imports everything for easy interactive use

---
+++ demo.py
@@ -0,0 +1,14 @@
+import ipdb
+
+from src import util
+from src import parsing
+from src import conversion
+from src.qualities import *
+from src.intervals import *
+from src.notes import *
+from src.chords import *
+from src.scales import *
+from src.keys import *
+from src.guitar import *
+from src.progressions import *
+from src.audio import *
