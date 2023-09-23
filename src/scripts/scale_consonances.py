fixed scale consonance calculation (and scale up-down playback while I'm at it)

src/scripts/scale_consonances.py
@@ -5,7 +5,7 @@ from src.display import DataFrame
 all_consonances = {}
 for name, factors in scales.canonical_scale_name_factors.items():
     sc = Scale(name)
-    all_consonances[sc] = sc.consonance
+    all_consonances[sc] = sc.get_consonance(raw=True)
 
 sorted_scales = sorted(all_consonances, key=lambda x: all_consonances[x], reverse=True)
 cons_names = [sc.name for sc in sorted_scales]
@@ -13,16 +13,23 @@ cons_values = [all_consonances[sc] for sc in sorted_scales]
 
 df_pent = DataFrame(['Scale Name',
                 'Consonance'])
+df_hept = DataFrame(['Scale Name',
+            'Consonance'])
 df_other = DataFrame(['Scale Name',
             'Consonance'])
 for scale, cons in zip(sorted_scales, cons_values):
     if scale.is_pentatonic():
         df_pent.append([scale.name, round(cons,3)])
     elif scale.is_heptatonic():
+        df_hept.append([scale.name, round(cons,3)])
+    else:
         df_other.append([scale.name, round(cons,3)])
 df_pent.show(title='Pentatonic scale consonances')
 print()
-df_other.show(title='Heptatonic scale consonances:')
+df_hept.show(title='Heptatonic scale consonances:')
+print()
+df_other.show(title='Other scale consonances:')
+
 
 import numpy as np
 print(f'Highest consonance: {np.max(cons_values):.05f} ({cons_names[np.argmax(cons_values)]})')